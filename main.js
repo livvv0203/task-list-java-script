456f749af223f61b28650176f85c6905ab2e25b6
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-06-29 21:24:35 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,74 @@
+window.addEventListener("load", () => {
+  // Get dom elements
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const list_element = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (e) => {
+    // Stop from refreshing the page
+    e.preventDefault();
+
+    const task = input.value;
+    if (!task) {
+      alert("Please fill out the Task!");
+      return;
+    }
+
+    const task_element = document.createElement("div");
+    task_element.classList.add("task");
+
+    const task_content_element = document.createElement("div");
+    task_content_element.classList.add("content");
+
+    task_element.appendChild(task_content_element);
+
+    const task_input_element = document.createElement("input");
+    task_input_element.classList.add("text");
+    task_input_element.type = "text";
+    task_input_element.value = task;
+    task_input_element.setAttribute('readonly', 'readonly');
+
+    task_content_element.appendChild(task_input_element);
+
+    // Create/Append Edit and Delete button to each Task Element
+    const task_actions_element = document.createElement('div');
+    task_actions_element.classList.add('actions');
+
+    const task_edit_element = document.createElement('button');
+    task_edit_element.classList.add('edit');
+    task_edit_element.innerHTML = 'Edit';
+
+    const task_delete_element = document.createElement('button');
+    task_delete_element.classList.add('delete');
+    task_delete_element.innerHTML = 'Delete';
+
+    task_actions_element.appendChild(task_edit_element);
+    task_actions_element.appendChild(task_delete_element);
+
+    task_element.appendChild(task_actions_element);
+
+    // Append newly created task to the tasks list
+    list_element.appendChild(task_element);
+
+    input.value = "";
+
+    // Edit Function
+    task_edit_element.addEventListener('click', () => {
+      if (task_edit_element.innerText.toLowerCase() == 'edit') {
+        task_input_element.removeAttribute('readonly');
+        task_input_element.focus();
+        task_edit_element.innerText = 'Save';
+      } else {
+        task_input_element.setAttribute('readonly', 'readonly');
+        task_edit_element.innerText = 'Edit';
+      }
+    });
+
+    // Delete Function
+    task_delete_element.addEventListener('click', () => {
+        list_element.removeChild(task_element);
+    });
+
+
+  });
+});
